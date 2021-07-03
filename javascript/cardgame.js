1492751f16c67c0fdd51c6d0fa648340f58c2460
jiya <jiya2002@gmail.com>
2021-07-03 13:48:10 +0530

    pharmacy

---
+++ javascript/cardgame.js
@@ -0,0 +1,25 @@
+var light=document.getElementById('first');
+
+light.addEventListener('click',function(light)
+{
+    light.target.classList.toggle('after');
+})
+
+var light1=document.getElementById('first1');
+
+light1.addEventListener('click',function(light1)
+{
+    light1.target.classList.toggle('after1');
+})
+
+var light2=document.getElementById('first2');
+light2.addEventListener('click',function(light2)
+{
+    light2.target.classList.toggle('after2');
+})
+
+var light3=document.getElementById('first3');
+light3.addEventListener('click',function(light3)
+{
+    light3.target.classList.toggle('after3');
+})
---
+++ php/my_project2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="my_project.css">
+    <title>Document</title>
+</head>
+<body>
+  <div class ="new">
+<ul>
+  <li><a class="active" href="my_project.html">Home</a></li>
+  <li><a href="my_project.html">Reset</a></li>
+  <li><a href="my_project.html">Go Back</a></li>
+</ul>
+<div class ="box1">
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat at reiciendis, alias, deleniti, iure eveniet corporis ex eaque cumque laborum nam! Neque illo dolores error quos quas mollitia ad dolorum!
+  </p>
+</div>
+</div>
+</body>
+</html>
